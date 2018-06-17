e957461b55e378470852a85d8301942fdeecde4f
Raina Wang <huan0924@gmail.com>
2018-06-17 14:58:50 -0700

    Build process

--- src/main/java/PlayGround.java
+++ src/main/java/PlayGround.java
@@ -2,7 +2,6 @@ package main.java;
 
 import main.java.io.rainaw.ruleStringParser.Exceptions.InvalidRuleStringFormatException;
 import main.java.io.rainaw.ruleStringParser.NestedRule;
-import main.java.io.rainaw.ruleStringParser.RuleEntity;
 import main.java.io.rainaw.ruleStringParser.RuleParser;
 
 public class PlayGround {
@@ -10,10 +9,11 @@ public class PlayGround {
     public static void main(String[] args) {
         RuleParser ruleParser = new RuleParser();
         try {
-            RuleEntity ruleSet1 = ruleParser.deserialize("(country == US) && (deviceName == PS4) && (appVersion >= 10.2)");
+            NestedRule ruleSet1 = ruleParser.deserialize("(country == US) && (deviceName == PS4) && (appVersion >= 10.2)");
 
             NestedRule ruleSet2 = ruleParser.deserialize("(country == US) && {!{(deviceName == PS4) && {(appVersion >= 102) || (appVersion <= 12)}} || {(deviceName == ANDROID) || (appVersion >= 42)}}");
 
+            System.out.println(ruleParser.serialize(ruleSet1));
             System.out.println(ruleParser.serialize(ruleSet2));
         } catch(InvalidRuleStringFormatException e) {
             System.out.println(e.getMessage());
